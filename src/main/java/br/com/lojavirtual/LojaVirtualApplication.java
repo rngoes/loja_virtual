68b134430fefc5cb723aedf786c43538c0b2ae0b
azurerngoes <azurerngoes@vmEstudos.uzcif5fo3epu1jd4pia4g321wf.ex.internal.cloudapp.net>
2023-06-15 23:41:23 -0300

    Criação de mais alguns modelos do sistema.

--- src/main/java/br/com/lojavirtual/LojaVirtualApplication.java
+++ src/main/java/br/com/lojavirtual/LojaVirtualApplication.java
@@ -2,8 +2,10 @@ package br.com.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "br.com.lojavirtual.model")
 public class LojaVirtualApplication {
 
 	public static void main(String[] args) {
