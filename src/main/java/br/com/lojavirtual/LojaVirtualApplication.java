c09932fabb2cde2d89618ec181205c906bc71557
azurerngoes <azurerngoes@vmEstudos.uzcif5fo3epu1jd4pia4g321wf.ex.internal.cloudapp.net>
2023-06-26 22:19:31 -0300

    Revert "Alterações da aula 3.1"
    This reverts commit ce7a8af836d680af4293b94fd98a9d9916c6514e.

--- src/main/java/br/com/lojavirtual/LojaVirtualApplication.java
+++ src/main/java/br/com/lojavirtual/LojaVirtualApplication.java
@@ -3,15 +3,9 @@ package br.com.lojavirtual;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EntityScan(basePackages = "br.com.lojavirtual.model")
-@ComponentScan(basePackages = {"br.com.*"})
-@EnableJpaRepositories(basePackages = {"br.com.lojavirtual.repository"})
-@EnableTransactionManagement
 public class LojaVirtualApplication {
 
 	public static void main(String[] args) {
--- src/test/java/br/com/lojavirtual/LojaVirtualApplicationTests.java
+++ src/test/java/br/com/lojavirtual/LojaVirtualApplicationTests.java
@@ -1,25 +1,13 @@
 package br.com.lojavirtual;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import br.com.lojavirtual.controller.AcessoController;
-import br.com.lojavirtual.model.Acesso;
-
-@SpringBootTest(classes = LojaVirtualApplication.class)
-public class LojaVirtualApplicationTests {
-
-	@Autowired
-	private AcessoController acessoController;
+@SpringBootTest
+class LojaVirtualApplicationTests {
 
 	@Test
-	public void testCadastraAcesso() {
-		Acesso acesso = new Acesso();
-
-		acesso.setDescricao("ROLE_ADMIN");
-
-		acessoController.salvarAcesso(acesso);
+	void contextLoads() {
 	}
 
 }
