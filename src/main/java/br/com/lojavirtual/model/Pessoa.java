bd0f6d20b86c371b23dc9b3849c677beb34f942b
azurerngoes <azurerngoes@vmEstudos.uzcif5fo3epu1jd4pia4g321wf.ex.internal.cloudapp.net>
2023-07-03 23:25:30 -0300

    Alterações do dia 03/07/2023

--- src/main/java/br/com/lojavirtual/model/Pessoa.java
+++ src/main/java/br/com/lojavirtual/model/Pessoa.java
@@ -37,6 +37,9 @@ public abstract class Pessoa implements Serializable {
 	@Column(nullable = false)
 	private String telefone;
 	
+	@Column
+	private String tipoPessoa;
+	
 	@OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	private List<Endereco> enderecos = new ArrayList<Endereco>();
 
@@ -72,6 +75,14 @@ public abstract class Pessoa implements Serializable {
 		this.telefone = telefone;
 	}
 	
+	public String getTipoPessoa() {
+		return tipoPessoa;
+	}
+
+	public void setTipoPessoa(String tipoPessoa) {
+		this.tipoPessoa = tipoPessoa;
+	}
+
 	public List<Endereco> getEnderecos() {
 		return enderecos;
 	}
